3f990ec824dfd12b490fe052737832d919d61e0a
rabitH5 <superstarpanda@gmail.com>
2012-11-13 19:04:06 -0500

    Moved class to a different package in ardor3d-awt to fix an issue with ProGuard.
    Moved class in:
    package com.ardor3d.image.util;
    to ->
    package com.ardor3d.image.util.awt;
    
    When merging modules with ProGuard, those class where clashing with reflection. To move them to a new package fixed the issue.

--- trunk/ardor3d-awt/src/main/java/com/ardor3d/image/util/awt/ScreenShotImageExporter.java
+++ trunk/ardor3d-awt/src/main/java/com/ardor3d/image/util/awt/ScreenShotImageExporter.java
@@ -8,7 +8,7 @@
  * LICENSE file or at <http://www.ardor3d.com/LICENSE>.
  */
 
-package com.ardor3d.image.util;
+package com.ardor3d.image.util.awt;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
