6eb737f382f2784a99cf9aef3c6c0dc49aecdc46
Julien Gouesse <gouessej@orange.fr>
2013-01-14 21:26:37 +0100

    Workaround for the deadlock occurring with Intel 3000HD under Windows when realizing a drawable and creating a context outside the AWT-EDT whereas the drawable is an heavyweight AWT GLCanvas

---
+++ ardor3d-jogl/src/main/java/com/ardor3d/framework/jogl/JoglInitializerRunnable.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2008-2010 Ardor Labs, Inc.
+ *
+ * This file is part of Ardor3D.
+ *
+ * Ardor3D is free software: you can redistribute it and/or modify it 
+ * under the terms of its license which may be found in the accompanying
+ * LICENSE file or at <http://www.ardor3d.com/LICENSE>.
+ */
+
+package com.ardor3d.framework.jogl;
+
+import com.ardor3d.framework.DisplaySettings;
+
+public class JoglInitializerRunnable implements Runnable {
+
+    private final JoglAwtCanvas _joglAwtCanvas;
+
+    private final DisplaySettings _settings;
+
+    public JoglInitializerRunnable(final JoglAwtCanvas joglAwtCanvas, final DisplaySettings settings) {
+        _joglAwtCanvas = joglAwtCanvas;
+        _settings = settings;
+    }
+
+    @Override
+    public void run() {
+        // Make the window visible to realize the OpenGL surface.
+        _joglAwtCanvas.setVisible(true);
+        // Force the realization
+        _joglAwtCanvas.display();
+        if (!_joglAwtCanvas.getDelegatedDrawable().isRealized()) {
+            throw new RuntimeException("The heavyweight AWT drawable cannot be realized");
+        }
+        // Request the focus here as it cannot work when the window is not visible
+        _joglAwtCanvas.requestFocus();
+        // The OpenGL context has been created after the realization of the surface
+        _joglAwtCanvas.getCanvasRenderer().setContext(_joglAwtCanvas.getContext());
+        // As the canvas renderer knows the OpenGL context, it can be initialized
+        _joglAwtCanvas.getCanvasRenderer().init(_settings, true);
+    }
+}
