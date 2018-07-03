953e2b118602e6314c20a9b88e499040b814e02d
Joshua Slack <renanse@gmail.com>
2018-07-03 15:38:37 -0500

    Eliminated a warning

--- ardor3d-core/src/test/java/com/ardor3d/renderer/queue/RenderBucketTypeTest.java
+++ ardor3d-core/src/test/java/com/ardor3d/renderer/queue/RenderBucketTypeTest.java
@@ -1,38 +1,39 @@
-package com.ardor3d.renderer.queue;
 
-import org.junit.Test;
+package com.ardor3d.renderer.queue;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.fail;
 
+import org.junit.Test;
+
 public class RenderBucketTypeTest {
 
-	@Test
-	public void getPrebucket() throws Exception {
-		RenderBucketType preBucket = RenderBucketType.getRenderBucketType("PreBucket");
-		assertNotNull(preBucket);
+    @Test
+    public void getPrebucket() throws Exception {
+        final RenderBucketType preBucket = RenderBucketType.getRenderBucketType("PreBucket");
+        assertNotNull(preBucket);
 
-		RenderBucketType preBucket2 = RenderBucketType.getRenderBucketType("PreBucket");
-		assertSame(preBucket, preBucket2);
-		assertSame(RenderBucketType.PreBucket, preBucket);
-	}
+        final RenderBucketType preBucket2 = RenderBucketType.getRenderBucketType("PreBucket");
+        assertSame(preBucket, preBucket2);
+        assertSame(RenderBucketType.PreBucket, preBucket);
+    }
 
-	@Test
-	public void getUserDefined() throws Exception {
-		RenderBucketType myBucket = RenderBucketType.getRenderBucketType("MyBucket");
-		assertNotNull(myBucket);
+    @Test
+    public void getUserDefined() throws Exception {
+        final RenderBucketType myBucket = RenderBucketType.getRenderBucketType("MyBucket");
+        assertNotNull(myBucket);
 
-		RenderBucketType myBucket2 = RenderBucketType.getRenderBucketType("MyBucket");
-		assertSame(myBucket, myBucket2);
-	}
+        final RenderBucketType myBucket2 = RenderBucketType.getRenderBucketType("MyBucket");
+        assertSame(myBucket, myBucket2);
+    }
 
-	@Test
-	public void getWithNull() throws Exception {
-		try {
-			RenderBucketType nullBucket = RenderBucketType.getRenderBucketType(null);
-			fail();
-		} catch (IllegalArgumentException e) {
-		}
-	}
+    @Test
+    public void getWithNull() throws Exception {
+        try {
+            RenderBucketType.getRenderBucketType(null);
+            fail();
+        } catch (final IllegalArgumentException e) {
+        }
+    }
 }
