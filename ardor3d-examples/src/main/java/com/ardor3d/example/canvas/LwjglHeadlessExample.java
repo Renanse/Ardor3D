9442ccb23f3a67d1ecb238acec366f090cfe69b8
Joshua Slack <renanse@gmail.com>
2018-11-29 11:00:32 -0600

    A simple attempt at an Headless GLFW canvas

---
+++ ardor3d-examples/src/main/java/com/ardor3d/example/canvas/LwjglHeadlessExample.java
@@ -0,0 +1,97 @@
+/**
+ * Copyright (c) 2008-2018 Ardor Labs, Inc.
+ *
+ * This file is part of Ardor3D.
+ *
+ * Ardor3D is free software: you can redistribute it and/or modify it
+ * under the terms of its license which may be found in the accompanying
+ * LICENSE file or at <http://www.ardor3d.com/LICENSE>.
+ */
+
+package com.ardor3d.example.canvas;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.logging.Logger;
+
+import com.ardor3d.example.ExampleBase;
+import com.ardor3d.example.Purpose;
+import com.ardor3d.framework.BasicScene;
+import com.ardor3d.framework.DisplaySettings;
+import com.ardor3d.framework.FrameHandler;
+import com.ardor3d.framework.lwjgl3.GLFWHeadlessCanvas;
+import com.ardor3d.framework.lwjgl3.Lwjgl3CanvasRenderer;
+import com.ardor3d.image.util.awt.AWTImageLoader;
+import com.ardor3d.image.util.awt.ScreenShotImageExporter;
+import com.ardor3d.input.Key;
+import com.ardor3d.input.logical.LogicalLayer;
+import com.ardor3d.util.Timer;
+import com.ardor3d.util.screen.ScreenExporter;
+
+/**
+ * This examples demonstrates how to render OpenGL (via LWJGL) on a SWT canvas via off-screen FBO rendering.
+ */
+@Purpose(htmlDescriptionKey = "com.ardor3d.example.canvas.LwjglHeadlessSwtExample", //
+        thumbnailPath = "com/ardor3d/example/media/thumbnails/canvas_LwjglHeadlessSwtExample.jpg", //
+        maxHeapMemory = 64)
+public class LwjglHeadlessExample {
+    private static final Logger logger = Logger.getLogger(LwjglHeadlessExample.class.toString());
+    private static RotatingCubeGame game;
+    private static ScreenShotImageExporter _screenShotExp = new ScreenShotImageExporter();
+    private static boolean _doShot = true;
+
+    public static void main(final String[] args) {
+        ExampleBase.addDefaultResourceLocators();
+
+        final Timer timer = new Timer();
+        final FrameHandler frameWork = new FrameHandler(timer);
+        final LogicalLayer logicalLayer = new LogicalLayer();
+
+        final AtomicBoolean exit = new AtomicBoolean(false);
+        final BasicScene scene = new BasicScene();
+        game = new RotatingCubeGame(scene, exit, logicalLayer, Key.T);
+
+        frameWork.addUpdater(game);
+
+        // Setup AWT image loader to load our ardor3d logo and font texture.
+        AWTImageLoader.registerLoader();
+
+        // set up the opengl canvas renderer to use
+        final Lwjgl3CanvasRenderer renderer = new Lwjgl3CanvasRenderer(scene);
+
+        // Create our headless canvas
+        final DisplaySettings settings = new DisplaySettings(800, 600, 24, 0);
+        final GLFWHeadlessCanvas canvas = new GLFWHeadlessCanvas(settings, renderer);
+        frameWork.addCanvas(canvas);
+
+        frameWork.init();
+
+        // game loop
+        while (!exit.get()) {
+            frameWork.updateFrame();
+            Thread.yield();
+            frameWork.updateFrame();
+            Thread.yield();
+            frameWork.updateFrame();
+            Thread.yield();
+            frameWork.updateFrame();
+            Thread.yield();
+
+            try {
+                Thread.sleep(100);
+                if (_doShot) {
+                    // force any waiting scene elements to be renderer.
+                    renderer.getRenderer().renderBuckets();
+                    ScreenExporter.exportCurrentScreen(renderer.getRenderer(), _screenShotExp);
+                    _doShot = false;
+                }
+            } catch (final InterruptedException ex) {
+                // TODO Auto-generated catch block
+                ex.printStackTrace();
+            }
+        }
+
+        // cleanup and exit
+        canvas.close();
+        System.exit(0);
+    }
+}
---
+++ ardor3d-lwjgl3/src/main/java/com/ardor3d/framework/lwjgl3/GLFWHeadlessCanvas.java
@@ -0,0 +1,194 @@
+/**
+ * Copyright (c) 2008-2018 Bird Dog Games, Inc.
+ *
+ * This file is part of Ardor3D.
+ *
+ * Ardor3D is free software: you can redistribute it and/or modify it
+ * under the terms of its license which may be found in the accompanying
+ * LICENSE file or at <http://www.ardor3d.com/LICENSE>.
+ */
+
+package com.ardor3d.framework.lwjgl3;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GL11C;
+
+import com.ardor3d.framework.CanvasRenderer;
+import com.ardor3d.framework.DisplaySettings;
+import com.ardor3d.framework.ICanvasListener;
+import com.ardor3d.framework.NativeCanvas;
+import com.ardor3d.image.Image;
+import com.ardor3d.input.MouseManager;
+import com.ardor3d.renderer.lwjgl3.Lwjgl3CanvasCallback;
+import com.ardor3d.util.Ardor3dException;
+import com.ardor3d.util.Constants;
+import com.ardor3d.util.stat.StatCollector;
+import com.ardor3d.util.stat.StatType;
+
+public class GLFWHeadlessCanvas implements NativeCanvas {
+    private static final Logger logger = Logger.getLogger(GLFWCanvas.class.getName());
+
+    protected final Lwjgl3CanvasRenderer _canvasRenderer;
+
+    protected final DisplaySettings _settings;
+    protected boolean _inited = false;
+
+    protected long _windowId;
+
+    protected GLFWErrorCallback _errorCallback;
+
+    /**
+     * If true, we will not try to drop and reclaim the context on each frame.
+     */
+    public static boolean SINGLE_THREADED_MODE = true;
+
+    public GLFWHeadlessCanvas(final DisplaySettings settings, final Lwjgl3CanvasRenderer canvasRenderer) {
+        _canvasRenderer = canvasRenderer;
+        _canvasRenderer.setCanvasCallback(new Lwjgl3CanvasCallback() {
+            @Override
+            public void makeCurrent(final boolean force) {
+                if (force || !SINGLE_THREADED_MODE) {
+                    GLFW.glfwMakeContextCurrent(_windowId);
+                }
+            }
+
+            @Override
+            public void releaseContext(final boolean force) {
+                if (force || !SINGLE_THREADED_MODE) {
+                    GLFW.glfwMakeContextCurrent(0);
+                }
+            }
+
+            @Override
+            public void doSwap() {
+                if (Constants.stats) {
+                    StatCollector.startStat(StatType.STAT_DISPLAYSWAP_TIMER);
+                }
+                GLFW.glfwSwapBuffers(_windowId);
+                if (Constants.stats) {
+                    StatCollector.endStat(StatType.STAT_DISPLAYSWAP_TIMER);
+                }
+            }
+        });
+        _settings = settings;
+    }
+
+    @Override
+    public void init() {
+        if (_inited) {
+            return;
+        }
+
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        if (!GLFW.glfwInit()) {
+            throw new IllegalStateException("Unable to initialize GLFW");
+        }
+
+        try {
+            GLFW.glfwDefaultWindowHints();
+            GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GL11C.GL_FALSE);
+            GLFW.glfwWindowHint(GLFW.GLFW_VISIBLE, GL11C.GL_FALSE);
+            GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MAJOR, 3);
+            GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MINOR, 3);
+            GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_DEBUG_CONTEXT, GLFW.GLFW_TRUE);
+            GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_PROFILE, GLFW.GLFW_OPENGL_CORE_PROFILE);
+            GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_FORWARD_COMPAT, GLFW.GLFW_TRUE);
+            GLFW.glfwSetErrorCallback(_errorCallback = GLFWErrorCallback.createPrint(System.err));
+            _windowId = GLFW.glfwCreateWindow(_settings.getWidth(), _settings.getHeight(), "Ardor3D", 0, 0);
+
+        } catch (final Exception e) {
+            logger.severe("Cannot create window");
+            logger.logp(Level.SEVERE, this.getClass().toString(), "initDisplay()", "Exception", e);
+            throw new Ardor3dException("Cannot create window: " + e.getMessage());
+        }
+
+        _canvasRenderer.init(_settings, true); // true - do swap in renderer.
+        _inited = true;
+    }
+
+    @Override
+    public void draw(final CountDownLatch latch) {
+        if (!_inited) {
+            init();
+        }
+
+        _canvasRenderer.draw();
+
+        if (latch != null) {
+            latch.countDown();
+        }
+    }
+
+    public CanvasRenderer getCanvasRenderer() {
+        return _canvasRenderer;
+    }
+
+    protected MouseManager _manager;
+
+    @Override
+    public MouseManager getMouseManager() {
+        return _manager;
+    }
+
+    @Override
+    public void setMouseManager(final MouseManager manager) {
+        _manager = manager;
+    }
+
+    @Override
+    public void close() {
+        GLFW.glfwDestroyWindow(_windowId);
+        GLFW.glfwTerminate();
+    }
+
+    @Override
+    public boolean isActive() {
+        return false;
+    }
+
+    @Override
+    public boolean isClosing() {
+        return GLFW.glfwWindowShouldClose(_windowId);
+    }
+
+    @Override
+    public void setVSyncEnabled(final boolean enabled) {
+        GLFW.glfwSwapInterval(enabled ? 1 : 0);
+    }
+
+    @Override
+    public void setTitle(final String title) {}
+
+    public void setIcon(final Image[] iconImages) {}
+
+    @Override
+    public void moveWindowTo(final int locX, final int locY) {}
+
+    public long getWindowId() {
+        return _windowId;
+    }
+
+    @Override
+    public int getContentHeight() {
+        return 0;
+    }
+
+    @Override
+    public int getContentWidth() {
+        return 0;
+    }
+
+    @Override
+    public void addListener(final ICanvasListener listener) {}
+
+    @Override
+    public boolean removeListener(final ICanvasListener listener) {
+        return false;
+    }
+}
