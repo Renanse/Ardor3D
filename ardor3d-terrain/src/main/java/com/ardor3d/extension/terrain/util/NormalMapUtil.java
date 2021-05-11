9c50bbb048397a72539a5fb22712b906971596b0
Joshua Slack <renanse@gmail.com>
2021-05-11 13:17:30 -0500

    Fixed up the terrain normal generator

--- ardor3d-terrain/src/main/java/com/ardor3d/extension/terrain/util/NormalMapUtil.java
+++ ardor3d-terrain/src/main/java/com/ardor3d/extension/terrain/util/NormalMapUtil.java
@@ -22,7 +22,7 @@ public class NormalMapUtil {
   /**
    * Generate an image from the given terrain height data to be used as a source for terrain normal
    * maps.
-   * 
+   *
    * @param heightmap
    *          the base height data. Generally this is the most detailed height data available. It must
    *          be a square heightmap, with a side of "side" as passed below.
@@ -47,42 +47,17 @@ public class NormalMapUtil {
     for (z = 0; z < side; ++z) {
       for (x = 0; x < side; ++x) {
         if (x == 0 || z == 0 || x == side - 1 || z == side - 1) {
-          n.set(0, 0, 1);
+          n.set(0, 1, 0);
         } else {
-          // change across "x" from point to our "left" to point on our "right"
-          double dXh = heightmap[z * side + x - 1] - heightmap[z * side + x + 1];
-          if (dXh != 0) {
-            // alter by our height scale
-            dXh *= heightScale;
-            // determine slope of perpendicular line
-            final double slopeX = 2.0 * xGridSpacing / dXh;
-            // now plug into cos(arctan(x)) to get unit length vector
-            n.setX(Math.copySign(1.0 / Math.sqrt(1 + slopeX * slopeX), dXh));
-            n.setY(0);
-            n.setZ(Math.abs(slopeX * n.getX()));
-          } else {
-            n.set(0, 0, 1);
-          }
+          // height change across "x" from point to our "left" to point on our "right"
+          final double dXh = heightScale * (heightmap[z * side + x - 1] - heightmap[z * side + x + 1]);
 
-          // change across "z" from point "above" us to point "below" us
-          double dZh = heightmap[(z - 1) * side + x] - heightmap[(z + 1) * side + x];
-          if (dZh != 0) {
-            // alter by our height scale
-            dZh *= heightScale;
-            // determine slope of perpendicular line
-            final double slopeZ = 2.0 * zGridSpacing / dZh;
-            // now plug into cos(arctan(x)) to get unit length vector
-            n2.setX(0);
-            n2.setY(Math.copySign(1.0 / Math.sqrt(1 + slopeZ * slopeZ), dZh));
-            n2.setZ(Math.abs(slopeZ * n2.getY()));
-          } else {
-            n2.set(0, 0, 1);
-          }
+          // height change across "z" from point "above" us to point "below" us
+          final double dZh = heightScale * (heightmap[(z - 1) * side + x] - heightmap[(z + 1) * side + x]);
 
-          // add together the vectors across X and Z and normalize to get final normal
-          n.addLocal(n2).normalizeLocal();
+          n.set(dXh / (2 * xGridSpacing), 1, dZh / (2 * zGridSpacing));
+          n.normalizeLocal();
         }
-        // System.err.println(n);
         data.put(3 * (z * side + x) + 0, (byte) ((int) (127 * n.getX()) + 128));
         data.put(3 * (z * side + x) + 1, (byte) ((int) (127 * n.getY()) + 128));
         data.put(3 * (z * side + x) + 2, (byte) ((int) (127 * n.getZ()) + 128));
