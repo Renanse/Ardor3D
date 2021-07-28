d027f4866c4fc12dee14a9e38f077574a124a948
Joshua Slack <renanse@gmail.com>
2021-07-28 17:52:26 -0500

    Force non-float backed items to load through AWT's int-based RGB pipeline in order to get channel order correct.

--- ardor3d-awt/src/main/java/com/ardor3d/image/util/awt/AWTImageLoader.java
+++ ardor3d-awt/src/main/java/com/ardor3d/image/util/awt/AWTImageLoader.java
@@ -11,15 +11,15 @@
 package com.ardor3d.image.util.awt;
 
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
 import java.awt.image.DataBuffer;
-import java.awt.image.DataBufferByte;
 import java.awt.image.DataBufferFloat;
-import java.awt.image.DataBufferInt;
-import java.awt.image.DataBufferShort;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 import javax.imageio.ImageIO;
 
@@ -29,12 +29,14 @@ import com.ardor3d.image.ImageDataFormat;
 import com.ardor3d.image.PixelDataType;
 import com.ardor3d.image.loader.ImageLoader;
 import com.ardor3d.image.loader.ImageLoaderUtil;
+import com.ardor3d.renderer.state.TextureState;
 import com.ardor3d.util.Ardor3dException;
 
 /**
  * Image loader that makes use of AWT's ImageIO to load image file data.
  */
 public class AWTImageLoader implements ImageLoader {
+  private static final Logger logger = Logger.getLogger(AWTImageLoader.class.getName());
   private static boolean createOnHeap = false;
 
   private static String[] supportedFormats;
@@ -70,143 +72,64 @@ public class AWTImageLoader implements ImageLoader {
       return null;
     }
 
+    final ColorModel colorModel = image.getColorModel();
+    final int numComponents = colorModel.getColorSpace().getNumComponents();
     final var dataBuffer = image.getRaster().getDataBuffer();
 
-    if (dataBuffer instanceof DataBufferByte) {
-      return convertDataBuffer((DataBufferByte) dataBuffer, image, flipImage);
-    }
-
-    if (dataBuffer instanceof DataBufferShort) {
-      return convertDataBuffer((DataBufferShort) dataBuffer, image, flipImage);
-    }
-
-    if (dataBuffer instanceof DataBufferInt) {
-      return convertDataBuffer((DataBufferInt) dataBuffer, image, flipImage);
-    }
-
+    // special case for float textures - should be 1 component
     if (dataBuffer instanceof DataBufferFloat) {
+      if (numComponents != 1) {
+        throw new Ardor3dException(
+            "Unhandled buffered image - could not convert float data buffer with " + numComponents + " components.");
+      }
+
       return convertDataBuffer((DataBufferFloat) dataBuffer, image, flipImage);
     }
 
-    throw new Ardor3dException(
-        "Unhandled buffered image - could not convert data buffer of type: " + dataBuffer.getClass().getName());
-  }
+    final boolean hasAlpha = colorModel.hasAlpha();
+    final boolean grayscale = colorModel.getNumComponents() == 1;
 
-  protected static Image convertDataBuffer(final DataBufferByte dataBuffer, final BufferedImage source,
-      final boolean flipImage) {
-    final var width = source.getWidth();
-    final var height = source.getHeight();
-    final var data = dataBuffer.getData();
-    final var colorModel = source.getColorModel();
-    final var bpp = colorModel.getPixelSize();
-    final var rowWidth = width * bpp >> 3;
-
-    if (flipImage) {
-      // flip data in place
-      final var tmp = new byte[rowWidth];
-      for (int y = 0, maxY = height / 2; y < maxY; y++) {
-        // read line A into tmp
-        System.arraycopy(data, y * rowWidth, tmp, 0, rowWidth);
-        // copy line B over line A
-        System.arraycopy(data, (height - y - 1) * rowWidth, data, y * rowWidth, rowWidth);
-        // copy saved line A over line B
-        System.arraycopy(tmp, 0, data, (height - y - 1) * rowWidth, rowWidth);
+    // make sure we have an image that is of correct type and orientation to read from.
+    BufferedImage texToConvert;
+    if (flipImage || ((image).getType() != BufferedImage.TYPE_BYTE_GRAY
+        && (hasAlpha ? (image).getType() != BufferedImage.TYPE_4BYTE_ABGR
+            : (image).getType() != BufferedImage.TYPE_3BYTE_BGR))) {
+      // Obtain the image data.
+      try {
+        texToConvert =
+            new BufferedImage(image.getWidth(null), image.getHeight(null), grayscale ? BufferedImage.TYPE_BYTE_GRAY
+                : hasAlpha ? BufferedImage.TYPE_4BYTE_ABGR : BufferedImage.TYPE_3BYTE_BGR);
+      } catch (final IllegalArgumentException e) {
+        logger.warning("Problem creating buffered Image: " + e.getMessage());
+        return TextureState.getDefaultTextureImage();
       }
+
+      final int imageWidth = image.getWidth(null);
+      final int imageHeight = image.getHeight(null);
+      final int[] tmpData = new int[imageWidth];
+      int row = 0;
+      for (int y = imageHeight - 1; y >= 0; y--) {
+        image.getRGB(0, (flipImage ? row++ : y), imageWidth, 1, tmpData, 0, imageWidth);
+        texToConvert.setRGB(0, y, imageWidth, 1, tmpData, 0, imageWidth);
+      }
+
+    } else {
+      texToConvert = image;
     }
 
-    // create image byte buffer
-    final var byteBuffer = BufferUtils.createByteBuffer(data.length);
-    byteBuffer.put(data);
-    byteBuffer.flip();
-
+    // Get image as a byte buffer - note this corrects order of Alpha component from ARGB to RGBA
+    final byte data[] = asByteArray(texToConvert);
+    final ByteBuffer scratch =
+        createOnHeap ? BufferUtils.createByteBufferOnHeap(data.length) : BufferUtils.createByteBuffer(data.length);
+    scratch.clear();
+    scratch.put(data);
+    scratch.flip();
     final Image ardorImage = new Image();
-    ardorImage.setWidth(width);
-    ardorImage.setHeight(height);
-    ardorImage.setData(byteBuffer);
+    ardorImage.setDataFormat(grayscale ? ImageDataFormat.Red : hasAlpha ? ImageDataFormat.RGBA : ImageDataFormat.RGB);
     ardorImage.setDataType(PixelDataType.UnsignedByte);
-
-    // Figure out our type
-    if (colorModel.getNumColorComponents() == 3) {
-      ardorImage.setDataFormat(colorModel.hasAlpha() ? ImageDataFormat.RGBA : ImageDataFormat.RGB);
-    } else if (colorModel.getNumColorComponents() == 2) {
-      ardorImage.setDataFormat(ImageDataFormat.RG);
-    } else if (colorModel.getNumColorComponents() == 1) {
-      ardorImage.setDataFormat(ImageDataFormat.Red);
-    }
-
-    // return our new Ardor3d image
-    return ardorImage;
-  }
-
-  protected static Image convertDataBuffer(final DataBufferShort dataBuffer, final BufferedImage source,
-      final boolean flipImage) {
-    final var width = source.getWidth();
-    final var height = source.getHeight();
-    final var data = dataBuffer.getData();
-    final var colorModel = source.getColorModel();
-    final var bpp = colorModel.getPixelSize();
-    final var rowWidth = width * bpp >> 4;
-
-    if (flipImage) {
-      // flip data in place
-      final var tmp = new short[rowWidth];
-      for (int y = 0, maxY = height / 2; y < maxY; y++) {
-        // read line A into tmp
-        System.arraycopy(data, y * rowWidth, tmp, 0, rowWidth);
-        // copy line B over line A
-        System.arraycopy(data, (height - y - 1) * rowWidth, data, y * rowWidth, rowWidth);
-        // copy saved line A over line B
-        System.arraycopy(tmp, 0, data, (height - y - 1) * rowWidth, rowWidth);
-      }
-    }
-
-    // create image byte buffer
-    final var byteBuffer = BufferUtils.createByteBuffer(data.length * 2);
-    byteBuffer.asShortBuffer().put(data);
-
-    final Image ardorImage = new Image();
-    ardorImage.setWidth(width);
-    ardorImage.setHeight(height);
-    ardorImage.setData(byteBuffer);
-    ardorImage.setDataType(PixelDataType.UnsignedShort);
-    ardorImage.setDataFormat(ImageDataFormat.Red); // ??? Not sure here
-
-    // return our new Ardor3d image
-    return ardorImage;
-  }
-
-  protected static Image convertDataBuffer(final DataBufferInt dataBuffer, final BufferedImage source,
-      final boolean flipImage) {
-    final var width = source.getWidth();
-    final var height = source.getHeight();
-    final var data = dataBuffer.getData();
-    final var rowWidth = width;
-
-    if (flipImage) {
-      // flip data in place
-      final var tmp = new int[rowWidth];
-      for (int y = 0, maxY = height / 2; y < maxY; y++) {
-        // read line A into tmp
-        System.arraycopy(data, y * rowWidth, tmp, 0, rowWidth);
-        // copy line B over line A
-        System.arraycopy(data, (height - y - 1) * rowWidth, data, y * rowWidth, rowWidth);
-        // copy saved line A over line B
-        System.arraycopy(tmp, 0, data, (height - y - 1) * rowWidth, rowWidth);
-      }
-    }
-
-    // create image byte buffer
-    final var byteBuffer = BufferUtils.createByteBuffer(data.length * 4);
-    byteBuffer.asIntBuffer().put(data);
-
-    final Image ardorImage = new Image();
-    ardorImage.setWidth(width);
-    ardorImage.setHeight(height);
-    ardorImage.setData(byteBuffer);
-    ardorImage.setDataType(PixelDataType.UnsignedByte);
-    ardorImage.setDataFormat(ImageDataFormat.RGBA);
-
-    // return our new Ardor3d image
+    ardorImage.setWidth(texToConvert.getWidth());
+    ardorImage.setHeight(texToConvert.getHeight());
+    ardorImage.setData(scratch);
     return ardorImage;
   }
 
@@ -240,6 +163,7 @@ public class AWTImageLoader implements ImageLoader {
     ardorImage.setWidth(width);
     ardorImage.setHeight(height);
     ardorImage.setData(byteBuffer);
+    // NB: not too sure if bpp==16 is possible in java.awt
     ardorImage.setDataType(bpp == 16 ? PixelDataType.HalfFloat : PixelDataType.Float);
     ardorImage.setDataFormat(ImageDataFormat.Red);
 
