4f44d002c4063b5d9403924e313dcb91b4cb235c
Joshua Slack <renanse@gmail.com>
2022-05-05 21:01:23 -0500

    Add some additional comments

--- ardor3d-examples/src/main/java/com/ardor3d/example/ui/UICompassExample.java
+++ ardor3d-examples/src/main/java/com/ardor3d/example/ui/UICompassExample.java
@@ -67,9 +67,12 @@ public class UICompassExample extends ExampleBase {
   }
 
   public void initUI() {
-    // Set up a hud
+    // Set up a hud to control rendering our UI
     hud = new UIHud(_canvas);
 
+    // Here I am setting up a main panel and coming up with some size/padding values.
+    // The compass will be a main panel with a sub panel for compass elements, a subpanel for headings,
+    // and several labels.
     final int cWidth = 600, cHeight = 80, edgePadding = 75, topPadding = (int) (cHeight * .25),
         bottomPadding = (int) (cHeight * .30);
     final var compassPanel = new UIPanel("compass", new RectLayout());
@@ -78,7 +81,7 @@ public class UICompassExample extends ExampleBase {
     compassPanel.setHudXY((_canvas.getContentWidth() - cWidth) / 2, _canvas.getContentHeight() - (int) (cHeight * 1.2));
     compassPanel.setBackdrop(new SolidBackdrop(new ColorRGBA(0f, 0f, 0f, 1f)));
 
-    // Add an inner panel
+    // Add an inner panel for icons and other elements
     final var innerCompassPanel = new UIPanel("innerCompass", new RectLayout());
     innerCompassPanel
         .setLayoutData(new RectLayoutData(0.0, 0.0, 1.0, 1.0, topPadding, edgePadding, bottomPadding, edgePadding));
@@ -106,7 +109,7 @@ public class UICompassExample extends ExampleBase {
     rightLabel.setLayoutData(new RectLayoutData(1.0, .42, 1.0, 0.42, 0, 5 - edgePadding, 0, 5));
     compassPanel.add(rightLabel);
 
-    // Add edge decoration
+    // Add edge decoration - bad areas, etc.
     final int edgeWidth = 25;
     final var leftFarEdge = new UIPanel("farleft");
     leftFarEdge.setLayoutData(new RectLayoutData(0, 0, 0.0, 1.0, 0, 0, 0, -edgeWidth));
@@ -128,7 +131,7 @@ public class UICompassExample extends ExampleBase {
     rightFarEdge.setBackdrop(new SolidBackdrop(new ColorRGBA(1, 0, 0, 1f)));
     innerCompassPanel.add(rightFarEdge);
 
-    // Add some icon in compass
+    // Add some sort of "interest" icon to the compass
     final double iconLocPercent = .3;
     final int iconSize = 20;
     final Texture tex =
@@ -140,7 +143,7 @@ public class UICompassExample extends ExampleBase {
         -iconSize / 2, -iconSize / 2));
     innerCompassPanel.add(iconLabel);
 
-    // Add sun location
+    // Let's get really creative and add a circle to indicate where the sun is or something like that!
     final double sunLocPercent = .75;
     final int sunSize = 20;
     final var sunSpot = new UIPanel("sun");
@@ -148,7 +151,7 @@ public class UICompassExample extends ExampleBase {
     sunSpot.setBackdrop(new SolidDiskBackdrop(ColorRGBA.YELLOW));
     innerCompassPanel.add(sunSpot);
 
-    // Add directionLine
+    // Add a line to indicate maybe the heading of something, or a direction, etc.
     final var dirIndicator = new UIPanel("fwd");
     final double dirLocPercent = .6;
     final int dirLocWidth = 4;
@@ -157,11 +160,15 @@ public class UICompassExample extends ExampleBase {
     dirIndicator.setBackdrop(new SolidBackdrop(ColorRGBA.MAGENTA));
     innerCompassPanel.add(dirIndicator);
 
-    // Add compass degree text
+    // Ok, now lets add some heading values below the sub panel. We'll start by creating a cropping
+    // panel because we want the text to crop to the same width as our inner compass panel.
     final var cropPanel = new UIPanel("text crop", new RectLayout());
     cropPanel.setLayoutData(new RectLayoutData(0, 0, 1, 0, 2 - bottomPadding, edgePadding, 2, edgePadding));
     compassPanel.add(cropPanel);
 
+    // Here we are doing a very quick calculation of some places where heading text might go. In reality
+    // this would be some kind of calculation based on character facing direction, maybe camera viewing
+    // angle, etc.
     final double spacing = .2, offset = -.03;
     int index = 0;
     for (int d = -180; d <= 270; d += 90) {
@@ -173,6 +180,7 @@ public class UICompassExample extends ExampleBase {
       index++;
     }
 
+    // pack our compass panel to get things sized and laid out.
     compassPanel.pack();
     hud.add(compassPanel);
 
